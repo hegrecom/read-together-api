1ccb1621bb2708f9ecd1adef1a1f582b53fc470e
Taehwa Kang <hegrecomm@gmail.com>
2022-03-16 00:16:44 +0900

    feat: Encrypt user password when creating users

--- src/dtos/user_dto.rs
+++ src/dtos/user_dto.rs
@@ -1,12 +1,25 @@
-use rocket::serde::Deserialize;
+use rocket::serde::{de, Deserialize, Deserializer};
 use crate::models::users;
+use bcrypt;
 
 #[derive(Debug, Clone, Deserialize, Insertable)]
 #[serde(crate = "rocket::serde")]
 #[table_name = "users"]
 pub struct UserDto {
     pub email: String,
+    #[serde(deserialize_with = "encrypt_password")]
     pub password: String,
     pub name: String,
+}
+
+fn encrypt_password<'de, D>(deserializer: D) -> Result<String, D::Error>
+where D: Deserializer<'de> 
+{
+    let password: &str = Deserialize::deserialize(deserializer)?;
+
+    match bcrypt::hash_with_salt(password, 10, b"aaaaaaaaaaaaaaaa") {
+        Ok(result) => Ok(result.to_string()),
+        Err(error) => Err(error).map_err(de::Error::custom),
+    }
 }
 
