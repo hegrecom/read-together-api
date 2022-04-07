766639573c27b34693fb1894348c73c3442fbe13
Taehwa Kang <hegrecomm@gmail.com>
2022-04-07 23:18:24 +0900

    refactor: Move persistent data access to model from service

--- src/models/user.rs
+++ src/models/user.rs
@@ -1,5 +1,16 @@
 use chrono::NaiveDateTime;
+use diesel::expression_methods::ExpressionMethods;
+use rocket::http::Status;
+use diesel::query_dsl::{RunQueryDsl, QueryDsl};
 use rocket::serde::Serialize;
+use rocket::State;
+use validator::Validate;
+
+use crate::config::AppConfig;
+use crate::config::database::Db;
+use crate::dtos::UserDto;
+use crate::helpers::{ErrorResponse, ErrorFormatter};
+use crate::schema::users;
 
 #[derive(Debug, Clone, Serialize, Queryable)]
 #[serde(crate = "rocket::serde")]
@@ -12,5 +23,27 @@ pub struct User {
     pub name: String,
     pub created_at: Option<NaiveDateTime>,
     pub updated_at: Option<NaiveDateTime>,
+}
+
+impl User {
+    pub async fn find_by_email(db: &Db, email: String) -> Option<Self> {
+        db.run(move |conn|
+            users::table.filter(users::email.eq(&email)).first(conn).ok()
+        ).await
+    }
+
+    pub async fn create(db: &Db, mut user_dto: UserDto, app_config: &State<AppConfig>) -> Result<Self, ErrorResponse> {
+        user_dto.validate().map_err(|e| ErrorResponse::new(Status::BadRequest, ErrorFormatter::format_error(e)))?;
+        user_dto.encrypt_password(app_config.get_password_salt()).map_err(|e| ErrorResponse::new(Status::InternalServerError, e.to_string()))?;
+        db.run(move |conn|
+            match diesel::insert_into(users::table).values(&user_dto).execute(conn) {
+                Ok(_) => {
+                    users::table.filter(users::email.eq(&user_dto.email)).first::<User>(conn)
+                        .map_err(|e| ErrorResponse::new(Status::InternalServerError, e.to_string()))
+                },
+                Err(e) => Err(ErrorResponse::new(Status::InternalServerError, e.to_string())),
+            }
+        ).await
+    }
 }
 
--- src/services/user_creation_service.rs
+++ src/services/user_creation_service.rs
@@ -1,14 +1,10 @@
 use crate::config::AppConfig;
 use crate::config::database::Db;
 use crate::dtos::UserDto;
-use crate::helpers::{ErrorResponse, ErrorFormatter};
+use crate::helpers::ErrorResponse;
 use crate::models::User;
-use crate::schema::users;
-use diesel::query_dsl::{RunQueryDsl, QueryDsl};
-use diesel::expression_methods::ExpressionMethods;
 use rocket::State;
 use rocket::http::Status;
-use validator::Validate;
 
 pub struct UserCreationService {
     pub db: Db,
@@ -19,23 +15,11 @@ impl UserCreationService {
         UserCreationService { db }
     }
 
-    pub async fn run(&self, mut user_dto:UserDto, app_config: &State<AppConfig>) -> Result<User, ErrorResponse> {
-        user_dto.validate().map_err(|e| ErrorResponse::new(Status::BadRequest, ErrorFormatter::format_error(e)))?;
-        user_dto.encrypt_password(app_config.get_password_salt()).map_err(|e| ErrorResponse::new(Status::InternalServerError, e.to_string()))?;
-        self.db.run(move |conn| {
-            let existing_user: Option<User> = users::table.filter(users::email.eq(&user_dto.email)).first(conn).ok();
-            match existing_user {
-                Some(_) => Err(ErrorResponse::new(Status::Conflict, "The email is already taken".to_string())),
-                None => {
-                    match diesel::insert_into(users::table).values(&user_dto).execute(conn) {
-                        Ok(_) => {
-                            users::table.filter(users::email.eq(&user_dto.email)).first::<User>(conn)
-                                .map_err(|e| ErrorResponse::new(Status::InternalServerError, e.to_string()))
-                        },
-                        Err(e) => Err(ErrorResponse::new(Status::InternalServerError, e.to_string())),
-                    }
-                },
-            }
-        }).await
+    pub async fn run(&self, user_dto: UserDto, app_config: &State<AppConfig>) -> Result<User, ErrorResponse> {
+        let existing_user: Option<User> = User::find_by_email(&self.db, user_dto.email.clone()).await;
+        match existing_user {
+            Some(_) => Err(ErrorResponse::new(Status::Conflict, "The email has already taken".to_string())),
+            None => User::create(&self.db, user_dto, app_config).await,
+        }
     }
 }
